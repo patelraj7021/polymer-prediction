Correct masking procedure

nn_modules.py
@@ -24,14 +24,11 @@ class MultiHeadAttention(nn.Module):
     def scaled_dot_product_attention(self, Q, K, V, mask=None):
         # Calculate attention scores
         attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
-        
         # Apply mask if provided (useful for preventing attention to certain parts like padding)
         if mask is not None:
             attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
-        
         # Softmax is applied to obtain attention probabilities
         attn_probs = torch.softmax(attn_scores, dim=-1)
-        
         # Multiply by values to obtain the final output
         output = torch.matmul(attn_probs, V)
         return output
@@ -47,13 +44,11 @@ class MultiHeadAttention(nn.Module):
         return x.transpose(1, 2).contiguous().view(batch_size, seq_length, d*self.num_heads)
     
     def forward(self, Q, K, V, mask=None):
+        # Q = K = V        
         # Apply linear transformations and split heads
-        Q = self.W_q(Q)
-        K = self.W_k(K)
-        V = self.W_v(V)
-        Q = self.split_heads(Q)
-        K = self.split_heads(K)
-        V = self.split_heads(V)
+        Q = self.split_heads(self.W_q(Q))
+        K = self.split_heads(self.W_k(K))
+        V = self.split_heads(self.W_v(V))
         
         # Perform scaled dot-product attention
         attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
@@ -102,8 +97,8 @@ class EncoderLayer(nn.Module):
         self.norm2 = nn.LayerNorm(d_model)
         self.dropout = nn.Dropout(dropout)
         
-    def forward(self, x):
-        attn_output = self.self_attn(x, x, x)
+    def forward(self, x, mask=None):
+        attn_output = self.self_attn(x, x, x, mask)
         x = self.norm1(x + self.dropout(attn_output))
         ff_output = self.feed_forward(x)
         x = self.norm2(x + self.dropout(ff_output))
@@ -122,10 +117,20 @@ class TransformerEncoder(nn.Module):
         ])
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, src):
-        enc_output = self.dropout(self.positional_encoding(src))
+    def forward(self, x):
+        # kinda hacky?
+        # need to ask all pad tokens
+        mask = x.sum(dim=-1) < 1e-5
+        mask = mask.unsqueeze(1)
+        mask = mask.expand(x.shape[0], x.shape[-2], x.shape[-2])
+        mask_T = torch.transpose(mask, -1, -2)
+        mask = torch.logical_or(mask, mask_T)
+        # better to have as 0 and 1s for ONNX?
+        mask = torch.where(mask, torch.tensor(0), torch.tensor(1))
+        mask = mask.unsqueeze(1)
+        enc_output = self.dropout(self.positional_encoding(x))
         for enc_layer in self.encoder_layers:
-            enc_output = enc_layer(enc_output)
+            enc_output = enc_layer(enc_output, mask)
 
         return enc_output
 
@@ -183,8 +188,8 @@ class TransformerPredictor(nn.Module):
         input_size = d_model * max_seq_length
         self.ff_predictor = FFPredictor(input_size, pred_layer_sizes, dropout)
         
-    def forward(self, src):
-        enc_output = self.encoder(src)
+    def forward(self, x):
+        enc_output = self.encoder(x)
         # reshape to flatten for ff predictor
         flattened_output = torch.reshape(
             enc_output, 

train.py
@@ -9,6 +9,24 @@ import nn_modules
 import wandb
 
 
+class Config:
+    def __init__(self, d_out, d_qkv, d_pred, num_heads, num_layers, num_pred_layers,
+                 dropout, lr, batch_size, epochs, optimizer, device, model_type, loss_type):
+        self.d_out = d_out
+        self.dropout = dropout
+        self.d_qkv = d_qkv
+        self.num_heads = num_heads
+        self.num_layers = num_layers
+        self.d_pred = d_pred
+        self.num_pred_layers = num_pred_layers
+        self.lr = lr
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.optimizer = optimizer
+        self.device = device
+        self.model_type = model_type
+        self.loss_type = loss_type
+
 
 def train_test_split(x_tensor, y_tensor, test_size=0.2):
     indices = torch.randperm(len(x_tensor))
@@ -22,14 +40,13 @@ def train_test_split(x_tensor, y_tensor, test_size=0.2):
     
 def train_model(model, train_dataloader, validation_dataloader, optimizer, loss_fn, config, max_abs_vals):
     # wandb.watch(model, log="all", log_freq=100, criterion=loss_fn)
-    
+    broke = False
     for epoch in range(config.epochs):
         
         # Train
         model.train()
         total_loss = 0
         num_batches = 0
-        broke = False
         for batch_tokens, batch_y_true in train_dataloader:
             # might need to train in normalized space?
             batch_y_true = batch_y_true * max_abs_vals  # undo normalization
@@ -48,35 +65,37 @@ def train_model(model, train_dataloader, validation_dataloader, optimizer, loss_
             num_batches += 1
         
         if broke:
+            # to avoid wasting time on cases where the model breaks
             break
-        
-        # Calculate average training loss
-        avg_train_loss = total_loss / num_batches
-        
-        # Validation
-        model.eval()
-        with torch.no_grad():
-            validation_losses = []
-            for val_tokens, val_y_true in validation_dataloader:
-                val_y_pred = model(val_tokens) * max_abs_vals  # undo normalization
-                val_loss = loss_fn(val_y_pred, val_y_true)
-                validation_losses.append(val_loss.item())
+        else:
+            # Calculate average training loss
+            avg_train_loss = total_loss / num_batches
             
-            avg_validation_loss = sum(validation_losses) / len(validation_losses)
-        
-        if avg_validation_loss < 0.00001 or avg_train_loss < 0.00001:
-            # this indicates something broke
-            break
-        
-        wandb.log({
-            "train_loss": avg_train_loss,
-            "validation_wMAE": avg_validation_loss
-        }, step=epoch+1)
-        
-        print(
-            f"Epoch {epoch+1}/{config.epochs}, Training Loss: {avg_train_loss:.6f}, "
-            f"Validation wMAE: {avg_validation_loss:.6f}"
-        )
+            # Validation
+            model.eval()
+            with torch.no_grad():
+                val_losses = 0
+                num_batches = 0
+                for val_tokens, val_y_true in validation_dataloader:
+                    val_y_pred = model(val_tokens) * max_abs_vals  # undo normalization
+                    val_loss = loss_fn(val_y_pred, val_y_true)
+                    val_losses += val_loss.item()
+                    num_batches += 1
+                avg_validation_loss = val_losses / num_batches
+            
+            if avg_validation_loss < 0.00001 or avg_train_loss < 0.00001:
+                # this indicates something broke
+                break
+            
+            wandb.log({
+                "train_loss": avg_train_loss,
+                "validation_wMAE": avg_validation_loss
+            }, step=epoch+1)
+            
+            print(
+                f"Epoch {epoch+1}/{config.epochs}, Training Loss: {avg_train_loss:.6f}, "
+                f"Validation wMAE: {avg_validation_loss:.6f}"
+            )
         
     return
 
@@ -113,7 +132,13 @@ def make(config):
     validation_dataloader = DataLoader(validation_dataset, batch_size=config.batch_size, shuffle=False)
     
     # Create optimizer and loss function
-    optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
+    optimizer_dict = {
+        'Adam': torch.optim.Adam,
+        'AdamW': torch.optim.AdamW,
+        'RMSprop': torch.optim.RMSprop,
+        'SGD': torch.optim.SGD
+    }
+    optimizer = optimizer_dict[config.optimizer](model.parameters(), lr=config.lr)
     loss_fn = utils.wMAE
     
     return model, train_dataloader, validation_dataloader, optimizer, loss_fn, max_abs_vals
@@ -135,6 +160,30 @@ def sweep_wrapper(config=None):
         wandb.save("model.onnx")
 
 
+def test_config():
+    # for checking memory usage
+    config = Config(
+        d_out=5,
+        d_qkv=128,
+        d_pred=256,
+        num_heads=2,
+        num_layers=4,
+        num_pred_layers=3,
+        dropout=0.05,
+        lr=0.0001,
+        batch_size=128,
+        epochs=10,
+        optimizer='AdamW',
+        device='cuda' if torch.cuda.is_available() else 'cpu',
+        model_type='transformer_predictor',
+        loss_type='wMAE'
+    )
+    model, train_dataloader, validation_dataloader, optimizer, loss_fn, max_abs_vals = make(config)
+    torchinfo.summary(model, input_data=next(iter(validation_dataloader))[0])
+    return
+    
+
+
 if __name__ == "__main__":
     
     sweep_config = {
@@ -147,32 +196,35 @@ if __name__ == "__main__":
             'd_out': {
                 'value': 5
             },
-            'dropout': {
-                'values': [0.05, 0.1, 0.2]
-            },
             'd_qkv': { # d_query = d_key = d_value
                 'values': [16, 32, 64, 128]
             },
+            'd_pred': {
+                'values': [64, 128, 256]
+            },
             'num_heads': {
-                'values': [1, 2, 3, 4]
+                'values': [1, 2]
             },
             'num_layers': {
-                'values': [1, 2, 3, 4]
-            },
-            'd_pred': {
-                'values': [16, 32, 64, 128]
+                'values': [2, 3, 4]
             },
             'num_pred_layers': {
-                'values': [1, 2, 3, 4]
+                'values': [1, 2, 3]
+            },
+            'dropout': {
+                'value': 0.05
             },
             'lr': {
-                'values': [0.00001, 0.0001, 0.001, 0.01]
+                'values': [0.00001, 0.0001, 0.001]
             },
             'batch_size': {
-                'values': [16, 32, 64, 128]
+                'value': 128
             },
             'epochs': {
-                'value': 50
+                'value': 200
+            },
+            'optimizer': {
+                'value': 'AdamW'
             },
             'device': {
                 'value': 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -185,7 +237,9 @@ if __name__ == "__main__":
             }
         }
     }
-    # need to try different optimizers
-    # need to vary adam hyperparameters
+    # need to add pad masking, optimize positional encoding, try CNN arch, and add passthrough
+    # need to vary adamw hyperparameters
     sweep_id = wandb.sweep(sweep_config, entity='patelraj7021-team', project="polymer-prediction")
-    wandb.agent(sweep_id, function=sweep_wrapper, count=10)
+    wandb.agent(sweep_id, function=sweep_wrapper, count=50)
+    
+    # test_config()

utils.py
@@ -2,15 +2,13 @@ import torch
 import numpy as np
 import pandas as pd
 
+
+
 def character_tokenizer(data_column):
     max_seq_length_index = data_column.str.len().idxmax()
     max_seq_length = len(data_column.iloc[max_seq_length_index]) + 2 # +2 for bos and eos
     chars = set()
     # get all unique characters
-<<<<<<< HEAD
-=======
-    # should hord code this eventually
->>>>>>> 272b12735e0bc24196a17e385b0106c62ec2035f
     for row in data_column:
         chars = chars.union(set(row))
     i = 0
@@ -50,20 +48,12 @@ def character_tokenizer(data_column):
             record_tensor = torch.cat([record_tensor, pad_tensor], dim=0)
         result.append(record_tensor)
     result_tensor = torch.stack(result, dim=0)
-<<<<<<< HEAD
     return result_tensor
+
 
 # normalize the input tensor by the maximum value of each column
 def normalize(x, eps=1e-8):
     max_abs_vals = torch.max(torch.nan_to_num(torch.abs(x)), dim=0).values + eps
-=======
-    return result_tensor, len(char_index_map), max_seq_length
-
-# normalize the input tensor by the maximum value of each column
-def normalize(x, eps=1e-8):
-    # this doesn't work for negative values?
-    max_abs_vals = torch.max(torch.abs(torch.nan_to_num(x)), dim=0).values + eps
->>>>>>> 272b12735e0bc24196a17e385b0106c62ec2035f
     return torch.div(x, max_abs_vals), max_abs_vals
 
 
@@ -147,21 +137,13 @@ def wMAE_kaggle(solution, submission, row_id_column_name):
     return score(solution, submission, row_id_column_name)
 
 
-<<<<<<< HEAD
 def MSE(pred, target):
-=======
-def MSE_loss(pred, target):
->>>>>>> 272b12735e0bc24196a17e385b0106c62ec2035f
     diff = torch.pow(pred - target, 2)
     loss = torch.nanmean(diff)
     return loss
 
 
-<<<<<<< HEAD
 def MAE(pred, target):
-=======
-def MAE_loss(pred, target):
->>>>>>> 272b12735e0bc24196a17e385b0106c62ec2035f
     diff = torch.abs(pred - target)
     loss = torch.nanmean(diff)
     return loss